134a581987de72bfe1452c288d9f82402efed02e
Ajeet <ajeetbharti279@gmail.com>
2021-10-16 20:56:41 +0530

    string

---
+++ string.js
@@ -0,0 +1,93 @@
+// var str="Ajeet Kumar"
+// var greet=`Hello ${str}`;
+// console.log(greet)
+
+
+//escape
+// var str="Ajeet \nKumar"
+// console.log(str)
+
+// var str="Ajeet \tKumar"
+// console.log(str)
+
+// var str="Ajeet \\Kumar"
+// console.log(str) 
+
+// var str="Ajeet \"Kumar";
+// console.log(str)
+
+
+//length
+// var str="Ajeet Kumar";
+// console.log(str.length)
+// console.log(str[4])
+
+
+//concatenation of two string
+// let str1='Ajeet'
+// let str2='Kumar'
+// console.log(str1+' '+str2)
+
+// let str1='Ajeet'
+// let str2='Kumar'
+// if(str1==str2){
+//     console.log('Eqaul')
+// }else{
+//     console.log('Not Equal')
+// }
+
+// let str1='Ajeet'
+// let str2='Kumar'
+// let str3=str1.concat(' ',str2)
+// console.log(str3);
+
+
+//substr()
+// let str='This is javascript tutorial. It is good tutorial. it is good tutorial';
+// let substring =str.substr(8,19);
+// console.log(substring)  //javascript tutorial
+
+
+//substring()
+// let str='This is javascript tutorial. It is good tutorial. it is good tutorial';
+// let subst =str.substring(8,17);
+// console.log(subst)  //javascrip
+
+
+//indexOf()
+// let str='This is javascript tutorial. It is good tutorial. it is good tutorial';
+// let position=str.indexOf('good');
+// console.log(position)  //35
+
+// let str='This is javascript tutorial. It is good tutorial. it is good tutorial';
+// let position=str.indexOf('is',10);
+// console.log(position)  //32
+
+// let str='This is javascript tutorial. It is good tutorial. it is good tutorial';
+// let position=str.lastIndexOf('is');
+// console.log(position)  
+
+
+//trim()
+// let str=' This is javascript tutorial. It is good tutorial '
+// let trimStr=str.trim();
+// console.log(trimStr)  //trim,trimEnd,trimStart
+
+
+//toUpperCase or toLowerCase
+// let str=' This is javascript tutorial'
+// // let str1=str.toLowerCase();
+// let str1=str.toUpperCase();
+// console.log(str1);
+
+
+//replace()
+// let str='This is javascript tutorial'
+// let str1=str.replace('javascript','java');
+// console.log(str1)
+
+
+//includes()
+// let str='This is javascript tutorial'
+// let str1=str.includes('is');
+// console.log(str1)//true
